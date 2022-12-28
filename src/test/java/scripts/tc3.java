d9c2738b5bf0917149cb1a88d44fc5564eb37dd4
om <om@DESKTOP-N7KUQ8V>
2022-12-29 02:16:59 +0530

    project

---
+++ src/test/java/scripts/tc3.java
@@ -0,0 +1,15 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class tc3 {
+	@Test
+	public void demo() 
+	{ 
+		
+	
+	Reporter.log("hello java selenium",true);
+	}
+
+}
---
+++ src/test/java/scripts/tc4.java
@@ -0,0 +1,15 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class tc4 {
+	@Test
+	public void demo() 
+	{ 
+		
+	
+	Reporter.log("hello java python",true);
+	}
+
+}
